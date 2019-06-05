79f884c53c251bdb5de94bfcbf6a1e0a0fc1134d
sajal <sajalv128@gmail.com>
2019-06-05 23:02:15 +0200

    Add support for host app package name

--- app/src/main/java/com/rarecase/spring/MainActivity.java
+++ app/src/main/java/com/rarecase/spring/MainActivity.java
@@ -9,28 +9,36 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import java.util.List;
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        PackageManager packageManager = getPackageManager();
-        try {
-                    PackageInfo packageInfo = packageManager.getPackageInfo("com.saavn.android",0);
-                    if((packageInfo != null) && packageInfo.applicationInfo.enabled) {
+            super.onCreate(savedInstanceState);
+            PackageManager packageManager = getPackageManager();
+            List<PackageInfo> packageInfoList = packageManager.getInstalledPackages(PackageManager.GET_META_DATA);
+            for (PackageInfo packageInfo:packageInfoList) {
+                //App's package name has changed now, this is to support the new as well as the old app
+                //Anyone of them will do, if none of them are found, route to GooglePlay asking to download new package name
+                if(Objects.equals(packageInfo.packageName, "com.jio.media.jiobeats") || Objects.equals(packageInfo.packageName, "com.saavn.android")){
+                    if(packageInfo.applicationInfo.enabled) {
                         Log.i("MainActivity:","Found that app! Version: "+packageInfo.versionName);
                         startActivity(new Intent(this, TabActivity.class));
                         MainActivity.this.finish();
                     }else {
                         setContentView(R.layout.activity_main);
                     }
-        }catch (PackageManager.NameNotFoundException e) {
-            setContentView(R.layout.activity_main);
-        }
+                }
+                else{
+                    setContentView(R.layout.activity_main);
+                }
+            }
     }
 
     public void launchPlaystore(View view) {
-        Intent launchIntent= new Intent(Intent.ACTION_VIEW,Uri.parse("market://details?id=com.saavn.android"));//.setData(Uri.parse("market://details?id=com.saavn.android"));
+        Intent launchIntent= new Intent(Intent.ACTION_VIEW,Uri.parse("market://details?id=com.jio.media.jiobeats"));//.setData(Uri.parse("market://details?id=com.saavn.android"));
         startActivity(launchIntent);
     }
 }
